09104ec052e2350e764852c184da30e124b7bcc1
Юлия Носакова <yn@appkode.ru>
2018-06-20 16:30:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class UITestsDriver:
         desired_caps['deviceName'] = 'Android Emulator'
         desired_caps['app'] = PATH('/home/kode/android-ut-builds/Utair*.apk')
 
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
 
 class TestUM:
