ba9f5312e5f824ae5b0f6baa2c1719adf64df666
Yulia Nosakova <yn@appkode.ru>
2018-06-22 13:52:00 +0200

    experiments continued

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import pytest
+import os
+from os.path import isfile, join, abspath, dirname, getmtime
+from appium import webdriver
 from time import sleep
-from page import UITestsDriver, MainPage
+from page import MainPage
+
+# Returns abs path relative to this file and not cwd
+PATH = lambda p: abspath(
+    join(dirname(__file__), p)
+)
+
+
+def get_recent_file(mypath):
+    files_tuple = [(f, int(getmtime(join(mypath, f)))) for f in os.listdir(mypath) if isfile(join(mypath, f))]
+    sort_by_update = sorted(files_tuple, key=lambda file: file[1])
+    return sort_by_update[-1][0]
 
 
 class TestUM:
+    def setup_class(self):
+        recent_file = get_recent_file('/qa-builds')
+        full_path = '/home/kode/android-ut-builds/{}'.format(recent_file)
+        desired_caps = {
+            'platformName': 'Android',
+            'platformVersion': '7.0',
+            'deviceName': 'Android Emulator',
+            'app': PATH(full_path)
+        }
+        self.driver = webdriver.Remote('http://192.168.100.95:4723/wd/hub', desired_caps)
 
+    def tear_down(self):
+        self.driver.quit()
 
     def test_find_elements(self):
-        main_page = MainPage()
+        main_page = MainPage(self.driver)
         sleep(6)
         main_page.click_onboarding()
         main_page.swipe_onboarding()
--- page.py
+++ page.py
@@ -1,34 +1,12 @@
-import os
-from os.path import isfile, join, abspath, dirname, getmtime
-from appium import webdriver
 from locators import MainPageLocators
 
+class BasePage(object):
+    """Base class to initialize the base page that will be called from all pages"""
 
-# Returns abs path relative to this file and not cwd
-PATH = lambda p: abspath(
-    join(dirname(__file__), p)
-)
+    def __init__(self, driver):
+        self.driver = driver
 
-def get_recent_file(mypath):
-    files_tuple = [(f, int(getmtime(join(mypath, f)))) for f in os.listdir(mypath) if isfile(join(mypath, f))]
-    sort_by_update = sorted(files_tuple, key=lambda file: file[1])
-    return sort_by_update[-1][0]
-
-
-class UITestsDriver:
-    def __init__(self):
-        recent_file = get_recent_file('/qa-builds')
-        full_path = '/home/kode/android-ut-builds/{}'.format(recent_file)
-        desired_caps = {
-            'platformName': 'Android',
-            'platformVersion': '7.0',
-            'deviceName': 'Android Emulator',
-            'app': PATH(full_path)
-            }
-        self.driver = webdriver.Remote('http://192.168.100.95:4723/wd/hub', desired_caps)
-
-
-class MainPage(UITestsDriver):
+class MainPage(BasePage):
     """Home page action methods come here."""
 
     def click_onboarding(self):
@@ -40,8 +18,8 @@ class MainPage(UITestsDriver):
         size = self.driver.get_window_size()
         startx, starty = int(size['width']) * 0.8, int(size['height']) * 0.5
         endx, endy = int(size['width']) * 0.2, int(size['height']) * 0.5
-        self.driver.swipe(startx, starty, endx, endy, 4000)
+        self.driver.swipe(startx, starty, endx, endy, 300)
 
-class CheckinPage(UITestsDriver):
+class CheckinPage(BasePage):
     """Search results page action methods come here"""
     pass
