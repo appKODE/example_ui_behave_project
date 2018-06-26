0670b463e069160aa0468575253d3f3a154abae8
Yulia Nosakova <yn@appkode.ru>
2018-06-26 11:06:57 +0200

    locators searching based on provided bundle id

--- locators.py
+++ locators.py
@@ -1,4 +1,4 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
-    ONBOARDING_PAGE = (By.ID, 'com.appkode.utair.dev:id/onboardingPageImage')
+    ONBOARDING_PAGE = (By.ID, 'com.appkode.utair.{}:id/onboardingPageImage')
