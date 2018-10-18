90a236dbf54776d354e3ba59d5f5d2548a648502
Юлия Носакова <yn@appkode.ru>
2018-10-18 12:59:07 +0000

    Update configs.py

--- features/helpers/configs.py
+++ features/helpers/configs.py
@@ -4,5 +4,5 @@ DOCKER_LOCATION = '/qa-builds'
 ALPHA = 'com.appkode.utair.alpha'
 RELEASE = 'ru.utair.android'
 INTERNAL = 'com.appkode.utair.dev'
-CAPS = dict(platformName='Android', platformVersion='7.0', deviceName='Emulator')
+CAPS = dict(platformName='Android', platformVersion='6.0', deviceName='Emulator')
 SERVER = 'http://192.168.100.74:4723/wd/hub'
